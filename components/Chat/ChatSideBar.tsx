5a381538da4ba8a1ae1bd9cd1166306f86ff8bce
屑AI <imxieai@outlook.com>
2024-08-28 21:37:27 +0800

    Update ChatSideBar.tsx

--- components/Chat/ChatSideBar.tsx
+++ components/Chat/ChatSideBar.tsx
@@ -7,7 +7,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai'
 import { BiMessageDetail } from 'react-icons/bi'
 import { FiPlus } from 'react-icons/fi'
 import { FaStore } from 'react-icons/fa';
-import { FaRegFaceLaughWink } from 'react-icons/ri'
+import { FaRegFaceLaughWink } from 'react-icons/ra'
 import ChatContext from './chatContext'
 
 import './index.scss'
